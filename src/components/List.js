f01a005bb6b73237e55f1ae712da335603b1c448
juancrucarrau <juancruzcarrau1@gmail.com>
2022-10-06 17:44:50 -0300

    Presentacion de listas y tarjetas

--- src/components/List.js
+++ src/components/List.js
@@ -1,39 +1,48 @@
 import {Paper} from "@mui/material";
 import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import Card from "./Card";
 
-const List = ({data}) => {
+const List = ({listData}) => {
 
     const styles = {
         list: {
             minWidth: "300px",
-            minHeight: "calc(100% - 2 * 10px - 2px)",
+            maxHeight: "calc(100% - 2 * 10px - 2px)",
             padding: "10px",
             textAlign: "left",
-            borderRadius: "10px"
-        },
-        mainFlex: {
+            borderRadius: "10px",
             display: "flex",
             flexDirection: "column",
-            maxHeight: "100%"
+            backgroundColor: "rgb(255, 255, 255, 0.80)"
         },
         listTitle: {
             margin: "5px 10px 5px"
+        },
+        cardBox: {
+            display: "flex",
+            flexDirection: "column",
+            overflowY: "auto"
         }
     }
 
     return(
         <Paper sx={styles.list}>
-            <div style={styles.mainFlex}>
-                <div>
-                    <Typography variant="h6" sx={styles.listTitle}>
-                        {data.nombre}
-                    </Typography>
-                </div>
+            <Box>
+                <Typography variant="h6" sx={styles.listTitle}>
+                    {listData.nombre}
+                </Typography>
+            </Box>
 
-                <div>
-                </div>
-
-            </div>
+            <Box sx={styles.cardBox}>
+                {listData.tarjetas.map(card => {
+                    return(
+                        <div>
+                            <Card cardData={card}/>
+                        </div>
+                    )
+                })}
+            </Box>
         </Paper>
     )
 }
